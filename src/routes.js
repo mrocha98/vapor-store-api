0f9abc01dd01cc3446cdf8e268f8ab146cb49672
Matheus Rocha <matheus_rocha98@outlook.com>
2020-04-20 21:02:17 -0300

    feat: adding a root (/) route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
+const { NO_CONTENT } = require('http-status-codes')
 const routes = require('express').Router()
 const ProductController = require('./app/controllers/ProductController')
 const StockController = require('./app/controllers/StockController')
 
+routes.get('/', (req, res) => res.sendStatus(NO_CONTENT))
 routes.get('/products', ProductController.index)
 routes.get('/products/:id', ProductController.show)
 routes.get('/stock/:id', StockController.show)
